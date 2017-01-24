e86829e27313cf06ff1715d1da5c1140d1a6ff07
Chris Dalzell <cdalzell@gmail.com>
2017-01-23 22:13:00 -0500

    Now generating project names

---
+++ docs/js/generator.js
@@ -0,0 +1,48 @@
+function buildWordLists() {
+    console.log('building lists');
+    
+    const firstWords = adjectives.concat(adverbs).concat(alphabet).concat(verbs);
+    const lastWords = adjectives.concat(adverbs).concat(alphabet);
+
+    const lists = {
+        "firstWords" : firstWords,
+        "lastWords" : lastWords
+    };
+
+    return lists;
+}
+
+function getRandom(minimum, maximum) {
+    const min = Math.ceil(minimum);
+    const max = Math.floor(maximum);
+    const random = Math.random();
+
+    return Math.floor(random * (max - min)) + min;
+}
+
+function generateProjectName(wordLists) {
+    const firstWordPos = getRandom(0, wordLists.firstWords.length);
+    const firstWord = wordLists.firstWords[firstWordPos].toUpperCase();
+
+    let lastWord;
+
+    do {
+        const lastWordPos = getRandom(0, wordLists.lastWords.length);
+
+        lastWord = wordLists.lastWords[lastWordPos].toUpperCase();
+    } while (firstWord === lastWord)
+
+    return `${firstWord} ${lastWord}`;
+}
+
+$(document).ready(function docReady() {
+    $('#project-name').text('HAVE BLUE');
+
+    const wordLists = buildWordLists();
+
+    $("#generate").click(function handleGenerateClick() {
+        const projectName = generateProjectName(wordLists);
+
+        $('#project-name').text(projectName);
+    });
+});
